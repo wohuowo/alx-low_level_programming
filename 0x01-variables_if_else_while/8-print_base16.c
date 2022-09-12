baf4b5e38e02d4abbd15595617e8544ff4361286
Tony Obriku <tonyobriku@gmail.com>
2022-09-12 08:21:46 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,15 +5,14 @@
 * Return: 0
 **/
 int main(void)
-
 {
-    int i;
-    char x;
+int i;
+char x;
 
-    for (i = 0; i < 10; i++)
-    putchar(i + '0');
-    for (x = 'a'; x <= 'f'; x++)
-    putchar(x);
-    putchar('\n');
-    return (0);
+for (i = 0; i < 10; i++)
+putchar(i + '0');
+for (x = 'a'; x <= 'f'; x++)
+putchar(x);
+putchar('\n');
+return (0);
 }
